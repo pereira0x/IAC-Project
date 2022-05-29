5c10ccb66d80854ee89fd6e5719b31afd087e0d7
pearsettings44 <jose.a.pereira03@gmail.com>
2022-05-29 13:13:54 +0100

    add atraso

--- ProjIac.asm
+++ ProjIac.asm
@@ -13,7 +13,7 @@
 ; * - O teclado deve estar completamente funcional, detetando todas as teclas; Feito
 ; *
 ; * - Deve desenhar o rover e movimentá-lo para a esquerda e para a direita (de forma 
-; * contínua, enquanto se carrega na tecla), até atingir o limite do ecrã; 
+; * contínua, enquanto se carrega na tecla), até atingir o limite do ecrã; feito
 ; *
 ; * - Deve desenhar um meteoro (bom ou mau), no tamanho máximo, numa coluna 
 ; * qualquer, no topo do ecrã. Esse meteoro deve descer uma linha no ecrã sempre que se 
@@ -32,28 +32,28 @@
 ; *********************************************************************************
 TEC_LIN				EQU 0C000H	; endereço das linhas do teclado (periférico POUT-2)
 TEC_COL				EQU 0E000H	; endereço das colunas do teclado (periférico PIN)
-LINHA_TECLADO		EQU 8		; linha a testar (4ª linha, 1000b)
+LINHA_TECLADO			EQU 8		; linha a testar (4ª linha, 1000b)
 MASCARA				EQU 0FH		; para isolar os 4 bits de menor peso, ao ler as colunas do teclado
-TECLA_ESQUERDA		EQU 1		; tecla na primeira coluna do teclado (tecla C)
-TECLA_DIREITA		EQU 2		; tecla na segunda coluna do teclado (tecla D)
+TECLA_ESQUERDA			EQU 1		; tecla na primeira coluna do teclado (tecla C)
+TECLA_DIREITA			EQU 2		; tecla na segunda coluna do teclado (tecla D)
 
-DEFINE_LINHA    	EQU 600AH      ; endereço do comando para definir a linha
-DEFINE_COLUNA   	EQU 600CH      ; endereço do comando para definir a coluna
-DEFINE_PIXEL    	EQU 6012H      ; endereço do comando para escrever um pixel
-APAGA_AVISO     	EQU 6040H      ; endereço do comando para apagar o aviso de nenhum cenário selecionado
-APAGA_ECRÃ	 	    EQU 6002H      ; endereço do comando para apagar todos os pixels já desenhados
-SELECIONA_CENARIO_FUNDO  EQU 6042H ; endereço do comando para selecionar uma imagem de fundo
-TOCA_SOM			EQU 605AH      ; endereço do comando para tocar um som
+DEFINE_LINHA    		EQU 600AH      ; endereço do comando para definir a linha
+DEFINE_COLUNA   		EQU 600CH      ; endereço do comando para definir a coluna
+DEFINE_PIXEL    		EQU 6012H      ; endereço do comando para escrever um pixel
+APAGA_AVISO     		EQU 6040H      ; endereço do comando para apagar o aviso de nenhum cenário selecionado
+APAGA_ECRÃ	 		EQU 6002H      ; endereço do comando para apagar todos os pixels já desenhados
+SELECIONA_CENARIO_FUNDO  EQU 6042H      ; endereço do comando para selecionar uma imagem de fundo
 
-LINHA        	EQU  16         ; linha do boneco (a meio do ecrã))
-COLUNA			EQU  30         ; coluna do boneco (a meio do ecrã)
+LINHA        		EQU  27        ; linha do boneco (a meio do ecrã))
+COLUNA			EQU  30        ; coluna do boneco (a meio do ecrã)
 
-MIN_COLUNA		EQU  0		    ; número da coluna mais à esquerda que o objeto pode ocupar
-MAX_COLUNA		EQU  63         ; número da coluna mais à direita que o objeto pode ocupar
-ATRASO			EQU	400H		; atraso para limitar a velocidade de movimento do boneco
+MIN_COLUNA		EQU  0		; número da coluna mais à esquerda que o objeto pode ocupar
+MAX_COLUNA		EQU  63        ; número da coluna mais à direita que o objeto pode ocupar
+ATRASO			EQU	0F00H		; atraso para limitar a velocidade de movimento do boneco
 
-LARGURA		    EQU	5			; largura do boneco
-COR_PIXEL		EQU	0FF00H		; cor do pixel: vermelho em ARGB (opaco e vermelho no máximo, verde e azul a 0)
+LARGURA		EQU	5			; largura do Tubarao
+ALTURA		EQU 4			; Altura do Tubarao
+COR_PIXEL		EQU	 0FF00H		; cor do pixel: vermelho em ARGB (opaco e vermelho no máximo, verde e azul a 0)
 
 ; *********************************************************************************
 ; * Dados 
@@ -68,22 +68,24 @@ SP_inicial:				; este é o endereço (1200H) com que o SP deve ser
 							
 DEF_BONECO:					; tabela que define o boneco (cor, largura, pixels)
 	WORD		LARGURA
-	WORD		COR_PIXEL, 0, COR_PIXEL, 0, COR_PIXEL		; # # #   as cores podem ser diferentes
-     
-
+	WORD		ALTURA
+	WORD		0, 0, 0F39FH, 0, 0		; # # #   as cores podem ser diferentes
+    WORD		0F000H, 0F39FH, 0FFFFH, 0F39FH, 0F000H 
+	WORD		0F39FH, 0FFFFH, 0FF00H, 0FFFFH, 0F39FH
+	WORD		0FFFFH, 0FF00H, 0FC00H, 0FF00H, 0FFFFH
 ; *********************************************************************************
 ; * Código
 ; *********************************************************************************
-PLACE   0               ; o código tem de começar em 0000H
+PLACE   0                     ; o código tem de começar em 0000H
 inicio:
-	MOV  SP, SP_inicial	; inicializa SP para a palavra a seguir
+	MOV  SP, SP_inicial		; inicializa SP para a palavra a seguir
 						; à última da pilha
                             
-     MOV  [APAGA_AVISO], R1	            ; apaga o aviso de nenhum cenário selecionado (o valor de R1 não é relevante)
-     MOV  [APAGA_ECRÃ], R1	            ; apaga todos os pixels já desenhados (o valor de R1 não é relevante)
-	MOV	R1, 0			                ; cenário de fundo número 0
+     MOV  [APAGA_AVISO], R1	; apaga o aviso de nenhum cenário selecionado (o valor de R1 não é relevante)
+     MOV  [APAGA_ECRÃ], R1	; apaga todos os pixels já desenhados (o valor de R1 não é relevante)
+	MOV	R1, 0			; cenário de fundo número 0
      MOV  [SELECIONA_CENARIO_FUNDO], R1	; seleciona o cenário de fundo
-	MOV	R7, 1			                ; valor a somar à coluna do boneco, para o movimentar
+	MOV	R7, 1			; valor a somar à coluna do boneco, para o movimentar
      
 posição_boneco:
      MOV  R1, LINHA			; linha do boneco
@@ -91,43 +93,35 @@ posição_boneco:
 	MOV	R4, DEF_BONECO		; endereço da tabela que define o boneco
 
 mostra_boneco:
-	CALL	desenha_boneco	; desenha o boneco a partir da tabela
-
-espera_nao_tecla:			; neste ciclo espera-se até NÃO haver nenhuma tecla premida
-	MOV  R6, LINHA_TECLADO	; linha a testar no teclado
-	CALL	teclado			; leitura às teclas
-	CMP	R0, 0
-	JNZ	espera_nao_tecla	; espera, enquanto houver tecla uma tecla carregada
+	CALL	desenha_boneco		; desenha o boneco a partir da tabela
+	MOV R11, ATRASO
+	CALL atraso
 
 espera_tecla:				; neste ciclo espera-se até uma tecla ser premida
 	MOV  R6, LINHA_TECLADO	; linha a testar no teclado
 	CALL	teclado			; leitura às teclas
 	CMP	R0, 0
 	JZ	espera_tecla		; espera, enquanto não houver tecla
-	
-	MOV	R9, 0		    	; som com número 0
-	MOV [TOCA_SOM], R9		; comando para tocar o som
-	
 	CMP	R0, TECLA_ESQUERDA
 	JNZ	testa_direita
 	MOV	R7, -1			; vai deslocar para a esquerda
 	JMP	ve_limites
 testa_direita:
 	CMP	R0, TECLA_DIREITA
-	JNZ	espera_tecla	; tecla que não interessa
+	JNZ	espera_tecla		; tecla que não interessa
 	MOV	R7, +1			; vai deslocar para a direita
 	
 ve_limites:
 	MOV	R6, [R4]			; obtém a largura do boneco
-	CALL	testa_limites	; vê se chegou aos limites do ecrã e se sim força R7 a 0
+	CALL	testa_limites		; vê se chegou aos limites do ecrã e se sim força R7 a 0
 	CMP	R7, 0
 	JZ	espera_tecla		; se não é para movimentar o objeto, vai ler o teclado de novo
 
 move_boneco:
-	CALL	apaga_boneco	; apaga o boneco na sua posição corrente
+	CALL	apaga_boneco		; apaga o boneco na sua posição corrente
 	
 coluna_seguinte:
-	ADD	R2, R7			    ; para desenhar objeto na coluna seguinte (direita ou esquerda)
+	ADD	R2, R7			; para desenhar objeto na coluna seguinte (direita ou esquerda)
 
 	JMP	mostra_boneco		; vai desenhar o boneco de novo
 
@@ -141,23 +135,41 @@ coluna_seguinte:
 ;
 ; **********************************************************************
 desenha_boneco:
+	PUSH 	R1
 	PUSH	R2
 	PUSH	R3
 	PUSH	R4
 	PUSH	R5
-	MOV	R5, [R4]			; obtém a largura do boneco
-	ADD	R4, 2			    ; endereço da cor do 1º pixel (2 porque a largura é uma word)
+	PUSH 	R6
+	PUSH 	R7
+	PUSH	R8
+	MOV R8,R2
+	MOV	R7, [R4]			; obtém a largura do boneco
+	MOV R5,R7
+	ADD	R4, 2			
+	MOV	R6, [R4]			; obtém a altura do boneco	
+	ADD	R4, 2			; endereço da cor do 1º pixel (2 porque a largura é uma word)
 desenha_pixels:       		; desenha os pixels do boneco a partir da tabela
 	MOV	R3, [R4]			; obtém a cor do próximo pixel do boneco
-	CALL	escreve_pixel	; escreve cada pixel do boneco
-	ADD	R4, 2			    ; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
-     ADD  R2, 1             ; próxima coluna
-     SUB  R5, 1			    ; menos uma coluna para tratar
-     JNZ  desenha_pixels    ; continua até percorrer toda a largura do objeto
+	CALL	escreve_pixel		; escreve cada pixel do boneco
+	ADD	R4, 2			; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
+     ADD  R2, 1               ; próxima coluna
+     SUB  R5, 1			; menos uma coluna para tratar
+     JNZ  desenha_pixels      ; continua até percorrer toda a largura do objeto
+	;troca de linha
+	MOV R2,R8
+	MOV R5,R7
+	ADD R1,1
+	SUB  R6, 1
+	JNZ  desenha_pixels
+	POP R8 
+	POP R7
+	POP R6
 	POP	R5
 	POP	R4
 	POP	R3
 	POP	R2
+	POP R1
 	RET
 
 ; **********************************************************************
@@ -169,24 +181,43 @@ desenha_pixels:       		; desenha os pixels do boneco a partir da tabela
 ;
 ; **********************************************************************
 apaga_boneco:
+	PUSH 	R1
 	PUSH	R2
 	PUSH	R3
 	PUSH	R4
 	PUSH	R5
-	MOV	R5, [R4]			; obtém a largura do boneco
-	ADD	R4, 2			    ; endereço da cor do 1º pixel (2 porque a largura é uma word)
+	PUSH 	R6
+	PUSH 	R7
+	PUSH	R8
+	MOV R8,R2
+	MOV	R7, [R4]			; obtém a largura do boneco
+	MOV R5,R7
+	ADD	R4, 2			
+	MOV	R6, [R4]			; obtém a altura do boneco	
+	ADD	R4, 2			; endereço da cor do 1º pixel (2 porque a largura é uma word)
 apaga_pixels:       		; desenha os pixels do boneco a partir da tabela
-	MOV	R3, 0			    ; cor para apagar o próximo pixel do boneco
-	CALL	escreve_pixel	; escreve cada pixel do boneco
-	ADD	R4, 2			    ; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
-     ADD  R2, 1             ; próxima coluna
-     SUB  R5, 1			    ; menos uma coluna para tratar
+	MOV	R3, 0			; obtém a cor do próximo pixel do boneco
+	CALL	escreve_pixel		; escreve cada pixel do boneco
+	ADD	R4, 2			; endereço da cor do próximo pixel (2 porque cada cor de pixel é uma word)
+     ADD  R2, 1               ; próxima coluna
+     SUB  R5, 1			; menos uma coluna para tratar
      JNZ  apaga_pixels      ; continua até percorrer toda a largura do objeto
+	;troca de linha
+	MOV R2,R8
+	MOV R5,R7
+	ADD R1,1
+	SUB  R6, 1
+	JNZ  apaga_pixels
+	POP R8 
+	POP R7
+	POP R6
 	POP	R5
 	POP	R4
 	POP	R3
 	POP	R2
+	POP R1
 	RET
+
 
 
 ; **********************************************************************
@@ -198,7 +229,7 @@ apaga_pixels:       		; desenha os pixels do boneco a partir da tabela
 ; **********************************************************************
 escreve_pixel:
 	MOV  [DEFINE_LINHA], R1		; seleciona a linha
-	MOV  [DEFINE_COLUNA], R2	; seleciona a coluna
+	MOV  [DEFINE_COLUNA], R2		; seleciona a coluna
 	MOV  [DEFINE_PIXEL], R3		; altera a cor do pixel na linha e coluna já selecionadas
 	RET
 
@@ -233,19 +264,19 @@ testa_limite_esquerdo:		; vê se o boneco chegou ao limite esquerdo
 	MOV	R5, MIN_COLUNA
 	CMP	R2, R5
 	JGT	testa_limite_direito
-	CMP	R7, 0			    ; passa a deslocar-se para a direita
+	CMP	R7, 0			; passa a deslocar-se para a direita
 	JGE	sai_testa_limites
 	JMP	impede_movimento	; entre limites. Mantém o valor do R7
 testa_limite_direito:		; vê se o boneco chegou ao limite direito
-	ADD	R6, R2		    	; posição a seguir ao extremo direito do boneco
+	ADD	R6, R2			; posição a seguir ao extremo direito do boneco
 	MOV	R5, MAX_COLUNA
 	CMP	R6, R5
 	JLE	sai_testa_limites	; entre limites. Mantém o valor do R7
-	CMP	R7, 0		    	; passa a deslocar-se para a direita
+	CMP	R7, 0			; passa a deslocar-se para a direita
 	JGT	impede_movimento
 	JMP	sai_testa_limites
 impede_movimento:
-	MOV	R7, 0			    ; impede o movimento, forçando R7 a 0
+	MOV	R7, 0			; impede o movimento, forçando R7 a 0
 sai_testa_limites:	
 	POP	R6
 	POP	R5
@@ -271,3 +302,4 @@ teclado:
 	POP	R3
 	POP	R2
 	RET
+
